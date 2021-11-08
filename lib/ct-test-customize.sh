80d84560923cb63bdfb7c5570312ffff891b67f4
Ryan Sjostrand <sjostrand@hpe.com>
2021-11-08 17:26:42 -0600

    Add script to auto-discovery chart version directories for chart testing

--- lib/ct-test-customize.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -exu
-
-yq --version
-
-cat $CT_CONFIG
--- lib/update-ct-config-with-chart-dirs.sh
+++ lib/update-ct-config-with-chart-dirs.sh
@@ -1,12 +1,28 @@
 #! /usr/bin/env bash
 
+set -xeo pipefail
+
+YQ=/Users/rysjostran/Downloads/yq_darwin_amd64
+
 CHARTS_BASE=$1
 if [[ -z "$CHARTS_BASE" ]]; then
-    "Error: Chart base path not provided"
+    echo "Error: Chart base path not provided"
     exit 1
 fi
 
-for CHART_PATH in $CHARTS_BASE/v*/*; do
-    echo "Cleaning chart: ${CHART_PATH}"
-    
+if [[ -z "$CT_CONFIG" ]]; then
+    echo "Warning: CT_CONFIG enviroment variable is not set default to ct.yaml"
+    CT_CONFIG="ct.yaml"
+fi
+
+echo "Orginial chart testing configuration"
+cat $CT_CONFIG
+
+for CHART_VERSION_PATH in $CHARTS_BASE/v*; do
+    echo "Adding chart version dir ${CHART_VERSION_PATH}"
+    export CHART_VERSION_PATH="$CHART_VERSION_PATH"
+    $YQ eval --inplace -P '.chart-dirs |= . + [env(CHART_VERSION_PATH)]' "$CT_CONFIG"
 done
+
+echo "Customized chart testing configuration"
+cat $CT_CONFIG
