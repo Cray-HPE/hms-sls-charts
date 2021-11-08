820602ee78e3b3f77ddac4d7f04d49e2c98cc312
Ryan Sjostrand <sjostrand@hpe.com>
2021-11-08 17:31:54 -0600

    Remove yq hack

--- lib/update-ct-config-with-chart-dirs.sh
+++ lib/update-ct-config-with-chart-dirs.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-set -xeo pipefail
+# YQ Version 4+ is required for this script
 
-YQ=/Users/rysjostran/Downloads/yq_darwin_amd64
+set -xeo pipefail
 
 CHARTS_BASE=$1
 if [[ -z "$CHARTS_BASE" ]]; then
@@ -21,7 +21,7 @@ cat $CT_CONFIG
 for CHART_VERSION_PATH in $CHARTS_BASE/v*; do
     echo "Adding chart version dir ${CHART_VERSION_PATH}"
     export CHART_VERSION_PATH="$CHART_VERSION_PATH"
-    $YQ eval --inplace -P '.chart-dirs |= . + [env(CHART_VERSION_PATH)]' "$CT_CONFIG"
+    yq eval --inplace -P '.chart-dirs |= . + [env(CHART_VERSION_PATH)]' "$CT_CONFIG"
 done
 
 echo "Customized chart testing configuration"
