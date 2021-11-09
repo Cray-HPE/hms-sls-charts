12b50e635d93a7e68e16434043292b09410ef47f
Ryan Sjostrand <sjostrand@hpe.com>
2021-11-08 20:47:43 -0600

    Fix typo

--- lib/update-ct-config-with-chart-dirs.sh
+++ lib/update-ct-config-with-chart-dirs.sh
@@ -19,7 +19,7 @@ echo "Orginial chart testing configuration"
 cat $CT_CONFIG
 
 for CHART_VERSION_PATH in $CHARTS_BASE/v*; do
-    echo "Adding chart version dir ${CHART_VERSION_PATH}"
+    echo "Adding chart version directory: ${CHART_VERSION_PATH}"
     export CHART_VERSION_PATH="$CHART_VERSION_PATH"
     yq eval --inplace -P '.chart-dirs |= . + [env(CHART_VERSION_PATH)]' "$CT_CONFIG"
 done
