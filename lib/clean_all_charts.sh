9d8309be7c6271dd2674545c62bfa5ec63e2fd84
Ryan Sjostrand <sjostrand@hpe.com>
2021-11-09 14:27:56 -0600

    Fix typo

--- lib/clean_all_charts.sh
+++ lib/clean_all_charts.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(dirname $SCRIPT_PATH)
 
 CHARTS_BASE=$1
 if [[ -z "$CHARTS_BASE" ]]; then
-    "Error: Chart base path not provided"
+    echo  "Error: Chart base path not provided"
     exit 1
 fi
 
